86a985998d37e832b22554736a60c1a52ccbcfdf
Lu-el <lubov.elunina@mail.ru>
2021-12-08 15:54:56 +0400

    Task93 added

---
+++ task-93/task-93.js
@@ -0,0 +1,29 @@
+function moveAll(fromList, toList) { 
+    let elements = document.querySelectorAll(`ul.${fromList} > li`);
+    let searchTable = document.querySelector(`.${toList}`);
+    if (elements.length) {
+        for (let elem of elements) {
+            let li = document.createElement('li');
+            li.style.listStyleType = 'none';
+            li.textContent = elem.textContent;
+            searchTable.append(li);
+            elem.remove();
+        }
+    } else {
+        alert(`Нет элементов в списке`)
+    }
+}
+
+function moveOne(inList) {
+    let selector = document.getSelection().toString();
+    if (selector) {
+        let searchTable = document.querySelector(`.${inList}`);
+        let li = document.createElement('li');
+        li.style.listStyleType = 'none';
+        li.innerHTML = (`${selector}`);
+        searchTable.append(li);
+        document.getSelection().deleteFromDocument();
+    } else {
+        alert('Не выбран элемент')
+    }
+}
