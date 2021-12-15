d367aace23d7e1252f0681eb187bf2d551bd37ee
Lu-el <lubov.elunina@mail.ru>
2021-12-15 15:39:10 +0400

    Gallery2

--- task-94/task-94.js
+++ task-94/task-94.js
@@ -1,15 +1,19 @@
 "use strict";
+
 const listOfActive = [
     'https://www.b17.ru/blog/101157/',    
-    'https://dic.academic.ru/dic.nsf/ruwiki/992753',
+    'https://wildfauna.ru/ploskie-chervi',
     'http://www.danaida.ru/',
     'https://wildfauna.ru/kuznechik',
 ]
 
+function closeWindow() {
+    window.close();
+}
 
 function moveTime() {
     let delta = 10;
-    let deadline = new Date(Date.parse(new Date()) + delta * 1000);
+    let deadline = new Date(Date.now() + delta * 1000);
     initializeClock("countdown", deadline);
 }
 
@@ -17,12 +21,16 @@ function moveSite(id) {
     let site = document.getElementById(id);
     let i = (listOfActive.findIndex(x => x == site.src));
 
-    if (i == 0 || i < (listOfActive.length - 1)) {
+    if (i < 0 && (listOfActive.length != 1 )) {
+        site.src = listOfActive[1];
+        console.log(i);
+        console.log(site.src);
+
+    } else if (i == 0 || i < (listOfActive.length - 1)) {
         site.src = listOfActive[i+1];
-        
-    } else { // i = listOfActive.length-1
+
+    } else {
         let btn = document.getElementsByClassName("lastbtn");
-        // site.src = "";
             
         for (let elem of btn) {
         elem.style.display = "inline-block";
@@ -63,8 +71,8 @@ function getTimeRemaining (endTime) {
     }
 }
 
-// let timeinterval = setInterval(updateClock, 1000);
 let timeoutId;
+
 function initializeClock(id, endTime) {
     let clock = document.getElementById(id);
     let minutesInterval = clock.querySelector(".minutes");
@@ -74,8 +82,8 @@ function initializeClock(id, endTime) {
         let t = getTimeRemaining (endTime);
 
         if (t.total >= 0) {
-            minutesInterval.innerHTML = ("0" + t.minutes).slice(-2);
-            secondsInterval.innerHTML = ("0" + t.seconds).slice(-2);
+            minutesInterval.innerHTML = `${t.minutes}`.padStart(2, 0);
+            secondsInterval.innerHTML = `${t.seconds}`.padStart(2, 0);
             timeoutId = window.setTimeout(updateClock, 1000);
         } else if (t.total <= 0) {
             moveSite("mainWindow");
@@ -88,9 +96,20 @@ function moveBack(id) {
     clearTimeout(timeoutId);
     let site = document.getElementById(id);
     let i = (listOfActive.findIndex(x => x == site.src)); 
-    (i <= 0) ? site.src = listOfActive[0]: site.src = listOfActive[(i-1)];  
+    if (i <= 0) {
+        site.src = listOfActive[0]
+     } else {
+        site.src = listOfActive[(i-1)];  
+     }
+    let btn = document.getElementsByClassName("lastbtn");
+    for (let elem of btn) {
+        elem.style.display = "none";
+    }
+
+    let time = document.getElementsByClassName("time");
+    for (let char of time) {
+        char.style.display = "inline-block";
+    }
 }
 
-function test1() {
-    moveTime()
-};
+
