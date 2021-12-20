cca2bd9b7f38e2de5d31910e04b7f126ab2fd7f7
Lu-el <lubov.elunina@mail.ru>
2021-12-20 15:50:55 +0400

    Merge branch 'student' of github.com:Lu-el/web-course into student

--- task-93/task-93.js
+++ task-93/task-93.js
@@ -1,6 +1,6 @@
-function moveAll(fromList, toList) { 
-    let elements = document.querySelectorAll(`select.${fromList} > option`);
-    let searchTable = document.querySelector(`.${toList}`);
+function moveAll(fromList, toList, part) { 
+    let elements = part.querySelectorAll(`select.${fromList} > option`);
+    let searchTable = part.querySelector(`.${toList}`);
     if (elements.length) {
         for (let elem of elements) {
             let option = document.createElement('option');
@@ -14,9 +14,9 @@ function moveAll(fromList, toList) {
 }
  
 
-function moveOne(inList) {
-    let checked = document.querySelectorAll(':checked');
-    let searchTable = document.querySelector(`.${inList}`);
+function moveOne(inList, part) {
+    let checked = part.querySelectorAll(':checked');
+    let searchTable = part.querySelector(`.${inList}`);
     if (checked.length) {
         for (let check of checked) {
             let option = document.createElement('option');
@@ -28,3 +28,33 @@ function moveOne(inList) {
         alert('Не выбран элемент')
     }
 }
+
+
+document.addEventListener('click', function(event) {
+    let target = event.target;
+    let targetParent = target.closest('.buttons');
+    let batterflyBox = target.closest('.batterfly');
+    if (!targetParent || !batterflyBox) return;
+    let actionTarget = target.dataset.action;
+
+    function changeBox(box) {
+        switch (actionTarget) {
+            case "allToRight":
+                moveAll('leftTable','rightTable', box);
+                break;
+            case "allToLeft":
+                moveAll('rightTable','leftTable', box);
+                break;
+            case "moveOneTorightTable":
+                moveOne("rightTable", box);
+                break;
+            case "moveOneToleftTable":
+                moveOne("leftTable", box);
+                break;
+        }
+    }
+    changeBox(batterflyBox);
+
+});
+
+
